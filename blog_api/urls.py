90ed4bd32b047a22cd84f580f88068c15429ca12
Luke Hinds <luke@redhat.com>
2021-11-05 18:55:55 +0000

    First commit
    Signed-off-by: Luke Hinds <luke@redhat.com>

---
+++ blog_api/urls.py
@@ -0,0 +1,10 @@
+# create django paths for blog_api
+from django.urls import path
+# from .views import PostList, PostDetail
+
+app_name = 'blog_api'
+
+urlpatterns = [
+    # path('<int:pk>/', PostList.as_view(), name='detailcreate'),
+    # path('', PostList.as_view(), name='listcreate'),
+]
